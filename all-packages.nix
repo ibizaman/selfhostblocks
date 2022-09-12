23e69232c415027f4099f139207adcde99e3573f
ibizaman <ibizapeanut@gmail.com>
2022-09-12 12:31:27 -0700

    uniformize pkgs by lowercasing them

--- all-packages.nix
+++ all-packages.nix
@@ -9,22 +9,22 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // self);
 
   self = rec {
-    PostgresDB = callPackage ./PostgresDB {};
+    PostgresDB = callPackage ./postgresdb {};
 
     CaddyConfig = callPackage ./caddy/config.nix {inherit utils;};
     CaddyService = callPackage ./caddy/unit.nix {inherit utils;};
     CaddySiteConfig = callPackage ./caddy/siteconfig.nix {inherit utils;};
     mkCaddySiteConfig = callPackage ./caddy/mksiteconfig.nix {inherit CaddySiteConfig;};
 
-    PHPFPMConfig = callPackage ./PHP-FPM/config.nix {inherit utils;};
-    PHPFPMService = callPackage ./PHP-FPM/unit.nix {inherit utils;};
-    PHPFPMSiteConfig = callPackage ./PHP-FPM/siteconfig.nix {inherit utils;};
-    mkPHPFPMSiteConfig = callPackage ./PHP-FPM/mksiteconfig.nix {inherit PHPFPMSiteConfig;};
+    PHPFPMConfig = callPackage ./php-fpm/config.nix {inherit utils;};
+    PHPFPMService = callPackage ./php-fpm/unit.nix {inherit utils;};
+    PHPFPMSiteConfig = callPackage ./php-fpm/siteconfig.nix {inherit utils;};
+    mkPHPFPMSiteConfig = callPackage ./php-fpm/mksiteconfig.nix {inherit PHPFPMSiteConfig;};
 
-    TtrssEnvironment = callPackage ./Ttrss/environment.nix {};
-    TtrssConfig = callPackage ./Ttrss/config.nix {};
-    TtrssUpdateService = callPackage ./Ttrss/update.nix {inherit utils;};
-    TtrssUpgradeDBService = callPackage ./Ttrss/dbupgrade.nix {};
+    TtrssEnvironment = callPackage ./ttrss/environment.nix {};
+    TtrssConfig = callPackage ./ttrss/config.nix {};
+    TtrssUpdateService = callPackage ./ttrss/update.nix {inherit utils;};
+    TtrssUpgradeDBService = callPackage ./ttrss/dbupgrade.nix {};
   };
 in
 self
