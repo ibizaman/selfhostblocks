0705ce1c47cdb873fbb86ef8b8d3779664a0c052
ibizaman <ibizapeanut@gmail.com>
2022-09-28 23:34:03 -0700

    add haproxy fastcgi option

--- haproxy/siteconfig.nix
+++ haproxy/siteconfig.nix
@@ -3,18 +3,38 @@
 }:
 { serviceName
 , serviceSocket
+, phpFastcgi ? false
+, phpDocroot ? null
+, phpIndex ? "index.php"
 }:
 
+let
+  backend =
+    if !phpFastcgi
+    then ''
+    backend ${serviceName}
+      mode http
+      option forwardfor
+      server ${serviceName}1 ${serviceSocket}
+    '' else ''
+    backend ${serviceName}
+      mode http
+      option forwardfor
+      use-fcgi-app ${serviceName}-php-fpm
+      server ${serviceName}1 ${serviceSocket} proto fcgi
+
+    fcgi-app ${serviceName}-php-fpm
+      log-stderr global
+      docroot ${phpDocroot}
+      index ${phpIndex}
+      path-info ^(/.+\.php)(/.*)?$
+    '';
+in
 {
   acl = ''
   acl acl_${serviceName} hdr_beg(host) ${serviceName}.
   use_backend ${serviceName} if acl_${serviceName}
   '';
 
-  backend = ''
-  backend ${serviceName}
-    mode http
-    option forwardfor
-    server ${serviceName}1 ${serviceSocket}
-  '';
+  inherit backend;
 }
