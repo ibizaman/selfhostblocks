e10c649b02ea4fa985fcbefd592d333c2ce4eda8
ibizaman <ibizapeanut@gmail.com>
2023-12-03 23:33:55 -0800

    fix ensure clauses in postgresql
    fixes #35

--- test/vm/postgresql.nix
+++ test/vm/postgresql.nix
@@ -11,7 +11,7 @@
       shb.postgresql.ensures = [
         {
           username = "me";
-          database = "mine";
+          database = "me";
         }
       ];
     };
@@ -25,10 +25,10 @@
         return "sudo -u me psql -U {user} {db} --command \"\"".format(user=user, db=database)
 
     with subtest("cannot login because of missing user"):
-        machine.fail(peer_cmd("me", "mine"), timeout=10)
+        machine.fail(peer_cmd("me", "me"), timeout=10)
 
     with subtest("cannot login with unknown user"):
-        machine.fail(peer_cmd("notme", "mine"), timeout=10)
+        machine.fail(peer_cmd("notme", "me"), timeout=10)
 
     with subtest("cannot login to unknown database"):
         machine.fail(peer_cmd("me", "notmine"), timeout=10)
@@ -53,7 +53,7 @@
       shb.postgresql.ensures = [
         {
           username = "me";
-          database = "mine";
+          database = "me";
         }
       ];
     };
@@ -70,16 +70,16 @@
         return "psql -h 127.0.0.1 -p {port} -U {user} {db} --command \"\"".format(user=user, db=database, port=port)
 
     with subtest("can login with provisioned user and database"):
-        machine.succeed(peer_cmd("me", "mine"), timeout=10)
+        machine.succeed(peer_cmd("me", "me"), timeout=10)
 
     with subtest("cannot login with unknown user"):
-        machine.fail(peer_cmd("notme", "mine"), timeout=10)
+        machine.fail(peer_cmd("notme", "me"), timeout=10)
 
     with subtest("cannot login to unknown database"):
         machine.fail(peer_cmd("me", "notmine"), timeout=10)
 
     with subtest("cannot login with tcpip"):
-        machine.fail(tcpip_cmd("me", "mine", "5432"), timeout=10)
+        machine.fail(tcpip_cmd("me", "me", "5432"), timeout=10)
     '';
   };
 
@@ -95,7 +95,7 @@
       shb.postgresql.ensures = [
         {
           username = "me";
-          database = "mine";
+          database = "me";
         }
       ];
     };
@@ -112,10 +112,10 @@
         return "psql -h 127.0.0.1 -p {port} -U {user} {db} --command \"\"".format(user=user, db=database, port=port)
 
     with subtest("cannot login without existing user"):
-        machine.fail(peer_cmd("me", "mine"), timeout=10)
+        machine.fail(peer_cmd("me", "me"), timeout=10)
 
     with subtest("cannot login with user without password"):
-        machine.fail(tcpip_cmd("me", "mine", "5432"), timeout=10)
+        machine.fail(tcpip_cmd("me", "me", "5432"), timeout=10)
     '';
   };
 
@@ -141,7 +141,7 @@
       shb.postgresql.ensures = [
         {
           username = "me";
-          database = "mine";
+          database = "me";
           passwordFile = "/run/dbsecret";
         }
       ];
@@ -159,13 +159,13 @@
         return "PGPASSWORD={password} psql -h 127.0.0.1 -p {port} -U {user} {db} --command \"\"".format(user=user, db=database, port=port, password=password)
 
     with subtest("can peer login with provisioned user and database"):
-        machine.succeed(peer_cmd("me", "mine"), timeout=10)
+        machine.succeed(peer_cmd("me", "me"), timeout=10)
 
     with subtest("can tcpip login with provisioned user and database"):
-        machine.succeed(tcpip_cmd("me", "mine", "5432", "secretpw"), timeout=10)
+        machine.succeed(tcpip_cmd("me", "me", "5432", "secretpw"), timeout=10)
 
     with subtest("cannot tcpip login with wrong password"):
-        machine.fail(tcpip_cmd("me", "mine", "5432", "oops"), timeout=10)
+        machine.fail(tcpip_cmd("me", "me", "5432", "oops"), timeout=10)
     '';
   };
 }
