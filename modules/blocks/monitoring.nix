5fd0b1dc833dfa08f3a61a3c65d9bcf6cfc0262d
ibizaman <ibizaman@tiserbox.com>
2026-05-24 22:18:01 +0200

    fluentbit: remove tracing

--- modules/blocks/monitoring.nix
+++ modules/blocks/monitoring.nix
@@ -629,13 +629,11 @@ in
                 host = "localhost";
                 port = config.services.loki.configuration.server.http_listen_port;
 
-                labels = lib.traceValSeq (
-                  lib.concatMapAttrsStringSep ", " (n: v: "${n}=${v}") {
-                    job = "systemd-journal";
-                    domain = cfg.domain;
-                    hostname = config.networking.hostName;
-                  }
-                );
+                labels = lib.concatMapAttrsStringSep ", " (n: v: "${n}=${v}") {
+                  job = "systemd-journal";
+                  domain = cfg.domain;
+                  hostname = config.networking.hostName;
+                };
 
                 label_keys = "$unit";
               }
